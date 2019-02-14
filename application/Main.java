0f7c58c05e4f44377baa35a5e3243bc76c4b0bfd
Maxime Defrances <maxime.defrances@hotmail.fr>
2019-02-14 21:13:11 +0100

    test graphe dans appli JFX

--- application/Main.java
+++ application/Main.java
@@ -22,6 +22,26 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		
+		double[][] matrice = {
+            {0,6,8,10},
+            {0,0,2,0},
+            {0,0,0,2},
+            {0,0,0,0}
+		};
+		int[] niveauxSommets = {0,1,2,3};
+		Graphe g = new Graphe(matrice,niveauxSommets);
+        
+        System.out.println(g.getNbNiveaux());
+        System.out.println(g.getNiveau(2)
+            .getSommets().get(0)
+            .getPrecedents().get(0).getOrigine().getNom());
+        System.out.println(g.getNiveau(2)
+            .getSommets().get(0)
+            .getPrecedents().get(1).getOrigine().getNom());
+        System.out.println(g.getNiveau(2)
+            .getSommets().get(0)
+            .getSuivants().get(0).getExtremite().getNom());
 		launch(args);
 	}
 }
