a6adbd37fce6de766c75d2c43b22f07a3e566ea8
Maxime Defrances <maxime.defrances@hotmail.fr>
2019-02-14 22:06:32 +0100

    tests nouveautés

---
+++ application/BorderPaneCircles.java
@@ -0,0 +1,10 @@
+package application;
+
+import java.util.ArrayList;
+
+import javafx.scene.layout.BorderPane;
+import model.Sommet;
+
+public class BorderPaneCircles extends BorderPane {
+	public ArrayList<Sommet> sommets;
+}
--- application/Main.java
+++ application/Main.java
@@ -2,18 +2,38 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Paint;
 import javafx.fxml.FXMLLoader;
+
+import javafx.scene.shape.*;
+
+import model.*;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("GraphDisplayer.fxml"));
+			System.out.println("start");
+			
+			 //Creating a circle object         
+	        Circle circle = new Circle();
+	        circle.setCenterX(20.0); 
+	        circle.setCenterY(20.0); 
+	        circle.setRadius(10.0);
+	        String color = "red";
+	        circle.setFill(Paint.valueOf(color));
+	        
+	        
+			BorderPaneCircles root = (BorderPaneCircles)FXMLLoader.load(getClass().getResource("GraphDisplayer.fxml"));
+			//Creating a Group object  
+		    //Group root = new Group(circle);
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Sample Application"); 
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -22,7 +42,7 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
-		
+		System.out.println("main");
 		double[][] matrice = {
             {0,6,8,10},
             {0,0,2,0},
@@ -32,16 +52,22 @@ public class Main extends Application {
 		int[] niveauxSommets = {0,1,2,3};
 		Graphe g = new Graphe(matrice,niveauxSommets);
         
-        System.out.println(g.getNbNiveaux());
-        System.out.println(g.getNiveau(2)
-            .getSommets().get(0)
-            .getPrecedents().get(0).getOrigine().getNom());
-        System.out.println(g.getNiveau(2)
+		System.out.println(g.getNbNiveaux());
+		Niveau currentNiv = g.getNiveau(2);
+		Sommet premierSommet = currentNiv.getSommets().get(0);
+		model.Arc premierArc = premierSommet.getPrecedents().get(0);
+
+        System.out.println(premierArc.getOrigine().getNom());
+		
+		System.out.println(g.getNiveau(2)
             .getSommets().get(0)
             .getPrecedents().get(1).getOrigine().getNom());
         System.out.println(g.getNiveau(2)
             .getSommets().get(0)
             .getSuivants().get(0).getExtremite().getNom());
+        
+       
+        
 		launch(args);
 	}
 }
