33383f72a4f721ee82eb8e4f79faaf808639810e
Maxime Defrances <maxime.defrances@hotmail.fr>
2019-02-15 22:33:01 +0100

    Ajout menuBar + fileSelecteur !

--- application/GraphController.java
+++ application/GraphController.java
@@ -1,11 +1,34 @@
 package application;
 
 
+import java.io.File;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class GraphController {
 	public model.Graphe currentGraph;
+	
+	//@FXML
+	//Menu menuFile;
+	
+	final FileChooser fileChooser = new FileChooser();
 
-	@FXML public MenuItem openfile;
+	@FXML
+	protected void handleSubmitButtonAction(ActionEvent event) {
+		System.out.println("coucou");
+		FileChooser fileChooser = new FileChooser();
+		File selectedFile = fileChooser.showOpenDialog(null);
+ 
+			if (selectedFile != null) {
+ 
+    			System.out.println("File selected: " + selectedFile.getName());
+			}
+			else {
+				System.out.println("File selection cancelled.");
+			}
+    }
 }
