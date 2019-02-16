2cf77a7bac497255d7c0b9e1f14eff85287d1853
Maxime Defrances <maxime.defrances@hotmail.fr>
2019-02-16 02:30:56 +0100

    csv parver v0

--- application/GraphController.java
+++ application/GraphController.java
@@ -2,33 +2,68 @@ package application;
 
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuItem;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 
 public class GraphController {
-	public model.Graphe currentGraph;
 	
-	//@FXML
-	//Menu menuFile;
+	 private static final char DEFAULT_SEPARATOR = ',';
+	 private static final int MAX_SOMMETS = 50;
 	
 	final FileChooser fileChooser = new FileChooser();
+	fileChooser.getExtensionFilters().addAll(
+	         new ExtensionFilter("CSV Files", "*.csv")
+	         
+			);
+
 
 	@FXML
 	protected void handleSubmitButtonAction(ActionEvent event) {
 		System.out.println("coucou");
-		FileChooser fileChooser = new FileChooser();
+		FileChooser fileChooser = new FileChooser(new ExtensionFilter("Filter for .csv files","*.csv"));
 		File selectedFile = fileChooser.showOpenDialog(null);
  
 			if (selectedFile != null) {
- 
-    			System.out.println("File selected: " + selectedFile.getName());
+    			System.out.println("File selected: " + selectedFile.getAbsolutePath());
+    			readCsv(selectedFile);
 			}
 			else {
 				System.out.println("File selection cancelled.");
 			}
     }
+	
+	
+	public void readCsv(File fileToRead){
+		ArrayList<ArrayList<Integer>> matrice=new ArrayList<ArrayList<Integer>>();
+		try {
+			Scanner scanner = new Scanner(fileToRead);
+			while (scanner.hasNext()) {
+				String csvLine=scanner.nextLine();
+				
+				//if empty, return!
+		        if (csvLine == null && csvLine.isEmpty()) {
+		            return;
+		        }
+		        
+		        int lastSeparatorIndex=0;
+		        int nextSeparatorIndex=0;
+		        for (int i=0;(csvLine.indexOf(DEFAULT_SEPARATOR,lastSeparatorIndex)>0)&&  (i<MAX_SOMMETS) ;  i++  ){
+		        	nextSeparatorIndex = csvLine.indexOf(",",lastSeparatorIndex);
+		        	System.out.println("valeur numero "+i+" : "+csvLine.substring(lastSeparatorIndex,nextSeparatorIndex));
+		        	
+		        	lastSeparatorIndex=nextSeparatorIndex+1;
+		        }
+		        	        }
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
